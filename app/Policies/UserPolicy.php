93278f779da5b222e2ac62714bc2425ec6de6a13
Mirco Baalmans <mircobaalmans@live.nl>
2025-04-09 13:57:06 +0200

    Add company-manager to create users

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,7 +12,7 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasRole('administrator');
+        return $user->hasAnyRole(['administrator','company-manager']);
     }
 
     /**
